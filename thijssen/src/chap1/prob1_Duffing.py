1f5a1a59f82090f7b05bb18577038ec807321d48
Daniel Jensen <danielsjensen1@gmail.com>
2012-01-10 21:47:32 -0500

    Minor reorganization of Duffing.

--- thijssen/src/chap1/prob1_Duffing.py
+++ thijssen/src/chap1/prob1_Duffing.py
@@ -1,13 +1,16 @@
-from numpy import cos, linspace
+import matplotlib.pyplot as plt
+from numpy import cos, linspace, pi
 from scipy.integrate import odeint
 
 
-m = 1e0
-a = 0.25
-b = 0.5
+class Duffing(object):
+    def __init__(self, m=1e0, a=.25e0, b=0.5e0):
+        pass
+
 gam = 0.1
 F0 = 1
 omega = 2e0
+T = 2 * pi / omega
 x0 = [0e0, 1e0]
 #  Part a
 def func(y, t):
@@ -19,3 +22,6 @@ def func(y, t):
 numpts = 100
 t = linspace(0, 10, numpts)
 x = odeint(func, x0, t)
+print x.shape
+plt.plot(t, x[:,0], label='position')
+plt.show()

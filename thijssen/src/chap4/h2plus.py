8438e12f3200681d8fbf166a50fb078024ded501
Daniel Jensen <danielsjensen1@gmail.com>
2012-02-13 15:25:07 -0500

    Started H2+ code.

---
+++ thijssen/src/chap4/h2plus.py
@@ -0,0 +1,51 @@
+from numpy import empty, empty_like, exp, pi, sqrt
+from chap3.prob03 import part_b as eigh
+
+
+class H2Plus(object):
+    def __init__(self, exponents):
+        self.exponents = exponents
+        self.S = empty((len(exponents),) * 2)
+        self.T = empty_like(self.S)
+        self.A = empty_like(self.S)
+        self.fill_arrays()
+    
+    def coulomb(self, alpha_p, alpha_q):
+        return -2 * pi / (alpha_p + alpha_q)
+    
+    def approx_eigfunc(self, n, r):
+        C = self.eigvecs[:, n - 1] #  Subtract 1 for 0-based indexing
+        f = 0
+        #  Form the eigenfunction using the correct linear combination 
+        #  of the basis functions.
+        for i, alpha in enumerate(self.exponents):
+            f += C[i] * exp(-alpha * r**2)
+        return f
+    
+    def approx_energy(self, n):
+        return self.eigvals[n - 1] #  Subtract 1 for 0-based indexing 
+    
+    def exact_eigfunc(self, n, r):
+        #  Currently only the ground state eigenfunction is implemented
+        return exp(-r) / sqrt(pi)
+    
+    def exact_energy(self, n):
+        return -1e0 / float(2 * n**2)
+    
+    def fill_arrays(self):
+        for i, alpha_p in enumerate(self.exponents):
+            for j, alpha_q in enumerate(self.exponents):
+                self.S[i, j] = self.overlap(alpha_p, alpha_q)
+                self.T[i, j] = self.kinetic(alpha_p, alpha_q)
+                self.A[i, j] = self.coulomb(alpha_p, alpha_q)
+        self.H = self.T + self.A
+    
+    def kinetic(self, alpha_p, alpha_q):
+        alpha_sum = alpha_p + alpha_q
+        return 3 * alpha_p * alpha_q * pi**(3 / 2e0) / alpha_sum**(5 / 2e0)
+    
+    def overlap(self, alpha_p, alpha_q):
+        return (pi / (alpha_p + alpha_q))**(3 / 2e0)
+    
+    def variational(self):
+        self.eigvals, self.eigvecs = eigh(self.H, self.S)
---
+++ thijssen/src/chap4/h2plus_plt.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+from numpy import array, linspace
+from chap4.h2plus import H2Plus
+
+
+exponents = array([13.00773, 1.962079, 0.444529, 0.1219492])
+hydrogen = H2Plus(exponents)
+hydrogen.variational()
+def print_energies():
+    for n in range(1, len(exponents)+1):
+        approx = hydrogen.approx_energy(n)
+        exact = hydrogen.exact_energy(n)
+        print('E{0} = {1:+.7e}, {2:.7e}'.format(n, approx, exact))
+def plot_groundstate():
+    r = linspace(0, 3, 100)
+    n = 1 #  Ground state
+    approx = hydrogen.approx_eigfunc(n, r)
+    exact = hydrogen.exact_eigfunc(n, r)
+    if approx[1] * exact[1] < 0:
+        approx *= -1e0
+    plt.plot(r, approx, 'r-', label='STO-4G')
+    plt.plot(r, exact, 'b--', label='STO')
+    plt.xlabel(r'$r$')
+    plt.ylabel(r'$\phi_{1s}$')
+    plt.legend()
+    plt.title('Ground-state Electronic Eigenfunction of Hydrogen Atom')
+#    plt.show()
+    plt.savefig('prob03plot.pdf')
+
+if __name__ == '__main__':
+    print_energies()
+    plot_groundstate()

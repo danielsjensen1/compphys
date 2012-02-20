7898f6033acdcf38595a3c4c9e63f2d5891267f1
Daniel Jensen <danielsjensen1@gmail.com>
2012-02-20 14:06:34 -0500

    Completed the h2+ molecule variational calculation.

--- thijssen/src/chap4/h2plus.py
+++ thijssen/src/chap4/h2plus.py
@@ -1,64 +1,92 @@
 from itertools import product
-from numpy import empty, empty_like, exp, pi, sqrt
+from numpy import dot, empty, empty_like, exp, pi, sqrt
+from scipy.special import erf
 from chap3.prob03 import part_b as eigh
 
 
-class Nucleus(object):
-    def __init__(self, position, charge, exponents):
-        self.position = position
+class Atom(object):
+    def __init__(self, charge, exponents, position):
         self.charge = charge
         self.exponents = exponents
+        self.position = position
+def F0(t):
+    if t < 1e-6:
+        return 1e0
+    else:
+        thalf = sqrt(t)
+        return sqrt(pi) / (2e0 * thalf) * erf(sqrt(t))
 
 class H2Plus(object):
-    def __init__(self, nuclei, N=1):
-        """Apply the variational principle to the H2+ molecule."""
-        self.nuclei = nuclei
+    def __init__(self, atoms, N=1):
+        """Apply the variational principle to the H2+ molecule.
+        
+        `atoms` - list or tuple of class `Atom`
+            All of the atoms in the molecule and their properties.
+        `N` - integer
+            Total number of electrons.
+        """
+        self.atoms = atoms
         self.N = N
-        print(sum(len(nucleus.exponents) for nucleus in nuclei))
-        self.bsize = sum(len(nucleus.exponents) for nucleus in nuclei)
+        self.bsize = sum(len(atom.exponents) for atom in atoms)
         self.S = empty((self.bsize,) * 2)
         self.T = empty_like(self.S)
         self.A = empty_like(self.S)
         self.fill_arrays()
     
-    def coulomb(self, alpha_p, alpha_q):
-        return -2 * pi / (alpha_p + alpha_q)
+    def all_orbs(self):
+        """Generator that loops over all orbitals in the basis set."""
+        i = 0
+        for atom in self.atoms:
+            for exponent in atom.exponents:
+                yield i, exponent, atom.position
+                i += 1
     
-    def approx_eigfunc(self, n, r):
+    def coulomb(self, alpha, beta, fac, R_AB_sqrd, R_P, R_C, Z_C):
+        R_PC = R_P - R_C
+        R_PC_sqrd = dot(R_PC, R_PC)
+        term1 = -2e0 * pi * Z_C / (alpha + beta)
+        term2 = exp(-fac * R_AB_sqrd)
+        term3 = F0((alpha + beta) * R_PC_sqrd)
+        return term1 * term2 * term3
+    
+    def approx_eigfunc(self, n, x):
         C = self.eigvecs[:, n - 1] #  Subtract 1 for 0-based indexing
         f = 0
         #  Form the eigenfunction using the correct linear combination 
         #  of the basis functions.
-        for i, alpha in enumerate(self.exponents):
+        for i, alpha, R in self.all_orbs():
+            r = x - R[0]
             f += C[i] * exp(-alpha * r**2)
         return f
     
     def approx_energy(self, n):
         return self.eigvals[n - 1] #  Subtract 1 for 0-based indexing 
     
-    def exact_eigfunc(self, n, r):
-        #  Currently only the ground state eigenfunction is implemented
-        return exp(-r) / sqrt(pi)
-    
     def exact_energy(self, n):
         return -1e0 / float(2 * n**2)
     
     def fill_arrays(self):
-        for n1, n2 in product(self.nuclei, repeat=2):
-            print(n1.exponents)
-        for i, alpha_p in enumerate(self.exponents):
-            for j, alpha_q in enumerate(self.exponents):
-                self.S[i, j] = self.overlap(alpha_p, alpha_q)
-                self.T[i, j] = self.kinetic(alpha_p, alpha_q)
-                self.A[i, j] = self.coulomb(alpha_p, alpha_q)
+        for (i, alpha, R_A), (j, beta, R_B) in product(self.all_orbs(), 
+                                                       repeat=2):
+            R_AB = R_A - R_B
+            R_AB_sqrd = dot(R_AB, R_AB)
+            R_P = (alpha  * R_A + beta * R_B) / (alpha + beta)
+            fac = alpha * beta / (alpha + beta)
+            self.S[i, j] = self.overlap(alpha, beta, fac, R_AB_sqrd)
+            self.T[i, j] = self.kinetic(alpha, beta, fac, R_AB_sqrd)
+            self.A[i, j] = sum(self.coulomb(alpha, beta, fac, R_AB_sqrd, 
+                                            R_P, atom.position, atom.charge)
+                               for atom in self.atoms)
         self.H = self.T + self.A
     
-    def kinetic(self, alpha_p, alpha_q):
-        alpha_sum = alpha_p + alpha_q
-        return 3 * alpha_p * alpha_q * pi**(3 / 2e0) / alpha_sum**(5 / 2e0)
+    def kinetic(self, alpha, beta, fac, R_AB_sqrd):
+        term1 = fac * (3e0 - 2e0 * fac * R_AB_sqrd)
+        term2 = (pi / (alpha + beta))**(1.5e0)
+        term3 = exp(-fac * R_AB_sqrd)
+        return term1 * term2 * term3
     
-    def overlap(self, alpha_p, alpha_q):
-        return (pi / (alpha_p + alpha_q))**(3 / 2e0)
+    def overlap(self, alpha, beta, fac, R_AB_sqrd):
+        return (pi / (alpha + beta))**(1.5e0) * exp(-fac * R_AB_sqrd) 
     
     def variational(self):
         self.eigvals, self.eigvecs = eigh(self.H, self.S)
--- thijssen/src/chap4/h2plus_plt.py
+++ thijssen/src/chap4/h2plus_plt.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 from numpy import array, linspace
-from chap4.h2plus import H2Plus, Nucleus
+from chap4.h2plus import H2Plus, Atom
 
 
 #  Place the atoms one bohr radius apart
@@ -8,30 +8,30 @@ R1 = array([-.5e0, 0e0, 0e0])
 R2 = -R1
 Z = 1e0
 exponents = array([13.00773, 1.962079, 0.444529, 0.1219492])
-nleft = Nucleus(R1, Z, exponents)
-nright = Nucleus(R2, Z, exponents)
-hydrogen = H2Plus((nleft, nright))
-hydrogen.variational()
+nleft = Atom(Z, exponents, R1)
+nright = Atom(Z, exponents, R2)
+h2plus = H2Plus((nleft, nright))
+h2plus.variational()
 def print_energies():
     for n in range(1, len(exponents)+1):
-        approx = hydrogen.approx_energy(n)
-        exact = hydrogen.exact_energy(n)
+        approx = h2plus.approx_energy(n)
+        exact = h2plus.exact_energy(n)
         print('E{0} = {1:+.7e}, {2:.7e}'.format(n, approx, exact))
 def plot_groundstate():
-    r = linspace(0, 3, 100)
+    x = linspace(-3, 3, 100)
     n = 1 #  Ground state
-    approx = hydrogen.approx_eigfunc(n, r)
-    exact = hydrogen.exact_eigfunc(n, r)
-    if approx[1] * exact[1] < 0:
-        approx *= -1e0
-    plt.plot(r, approx, 'r-', label='STO-4G')
-    plt.plot(r, exact, 'b--', label='STO')
+    approx = h2plus.approx_eigfunc(n, x)
+#    exact = hydrogen.exact_eigfunc(n, r)
+#    if approx[1] * exact[1] < 0:
+#        approx *= -1e0
+    plt.plot(x, approx, 'x-', label='STO-4G')
+#    plt.plot(r, exact, 'b--', label='STO')
     plt.xlabel(r'$r$')
     plt.ylabel(r'$\phi_{1s}$')
     plt.legend()
-    plt.title('Ground-state Electronic Eigenfunction of Hydrogen Atom')
-#    plt.show()
-    plt.savefig('prob03plot.pdf')
+    plt.title('Ground-state Electronic Eigenfunction of H2+ Molecule')
+    plt.show()
+#    plt.savefig('prob03plot.pdf')
 
 if __name__ == '__main__':
     print_energies()

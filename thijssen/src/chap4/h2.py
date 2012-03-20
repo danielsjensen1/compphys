67b12c54d04d58e22f8ce63fa3a9387fa3638350
Daniel Jensen <danielsjensen1@gmail.com>
2012-03-20 16:37:21 -0400

    Started h2 non ionized.

---
+++ thijssen/src/chap4/h2.py
@@ -0,0 +1,169 @@
+from itertools import combinations, product
+from numpy import dot, einsum, empty, empty_like, exp, ones, pi, sqrt
+from scipy.special import erf
+from chap3.prob03 import part_b as eigh
+
+
+class Atom(object):
+    def __init__(self, charge, exponents, position):
+        """
+        Store the basis information for each atom
+        
+        `charge` - integer
+            Nuclear charge of the atom.
+        `exponents` - 1D array of floats
+            The exponents determining the width of the Gaussian basis functions.
+        `position` - 1x3 array of floats
+            The location of the atom in Cartesian coordinates.
+        """
+        self.charge = charge
+        self.exponents = exponents
+        self.position = position
+
+def F0(t):
+    """F0 function described on page 75 of Thijssen equation 4.114."""
+    if t < 1e-6: #  limit as t->0 of F0 is 1e0
+        return 1e0
+    else:
+        thalf = sqrt(t)
+        return sqrt(pi) / (2e0 * thalf) * erf(sqrt(t))
+
+class H2(object):
+    def __init__(self, atoms, N=1):
+        """Apply the variational principle to the He atom.
+        
+        `atoms` - list or tuple of class `Atom`
+            All of the atoms in the molecule and their properties.
+        `N` - integer
+            Total number of electrons.
+        """
+        self.atoms = atoms
+        self.N = N
+        self.bsize = sum(len(atom.exponents) for atom in atoms)
+        self.S = empty((self.bsize,) * 2)
+        self.T = empty_like(self.S)
+        self.A = empty_like(self.S)
+        self.Q = empty((self.bsize,) * 4)
+        self.fill_arrays()
+
+    def all_orbs(self):
+        """Generator that loops over all orbitals in the basis set."""
+        i = 0
+        for atom in self.atoms:
+            for exponent in atom.exponents:
+                yield i, exponent, atom.position
+                i += 1
+
+    def eigfunc(self, n, x):
+        """
+        Form eigenfunction `n` using LCAO.
+        
+        Note that currently this only does a plot of the projection of
+        the eigenfunction onto the x axis (y = z = 0).
+        
+        `n` - positive integer
+            The eigenfunction index.
+        `x` - 1D array
+            The x grid for the projection of the eigenfunction.
+        """
+        C = self.eigvecs[:, n - 1] #  Subtract 1 for 0-based indexing
+        f = 0
+        for i, alpha, R in self.all_orbs():
+            r = x - R[0]
+            f += C[i] * exp(-alpha * r ** 2)
+        return f
+
+    def energy(self, n):
+        """Electronic energy only."""
+        return self.eigvals[n - 1] #  Subtract 1 for 0-based indexing
+
+    def energy_tot(self, n):
+        """Total energy ignoring the motion of the nuclei."""
+        return self.energy(n) + self.coulomb_nuclei()
+
+    def coulomb(self, alpha, beta, fac, R_AB_sqrd, R_P, R_C, Z_C):
+        """Coulomb matrix elements."""
+        R_PC = R_P - R_C
+        R_PC_sqrd = dot(R_PC, R_PC)
+        term1 = -2e0 * pi * Z_C / (alpha + beta)
+        term2 = exp(-fac * R_AB_sqrd)
+        term3 = F0((alpha + beta) * R_PC_sqrd)
+        return term1 * term2 * term3
+
+    def coulomb_nuclei(self):
+        """The electrostatic energy of the nuclei."""
+        combos = combinations(self.atoms, 2)
+        def dist(r1, r2):
+            r12 = r1 - r2
+            return sqrt(dot(r12, r12))
+        E = sum(atm1.charge * atm2.charge / dist(atm1.position, atm2.position)
+                for atm1, atm2 in combos)
+        return E
+
+    def fill_arrays(self):
+        """Fill the overlap, kinetic, and coulomb arrays."""
+        for (i, alpha, R_A), (j, beta, R_B) in product(self.all_orbs(),
+                                                       repeat=2):
+            R_AB = R_A - R_B
+            R_AB_sqrd = dot(R_AB, R_AB)
+            R_P = (alpha * R_A + beta * R_B) / (alpha + beta)
+            fac = alpha * beta / (alpha + beta)
+            self.S[i, j] = self.overlap(alpha, beta, fac, R_AB_sqrd)
+            self.T[i, j] = self.kinetic(alpha, beta, fac, R_AB_sqrd)
+            self.A[i, j] = sum(self.coulomb(alpha, beta, fac, R_AB_sqrd,
+                                            R_P, atom.position, atom.charge)
+                               for atom in self.atoms)
+        self.H = self.T + self.A
+#        for (p, alpha_p, R_p), (r, alpha_r, R_r), (q, alpha_q, R_q), \
+#        (s, alpha_s, R_s) in product(self.all_orbs(), repeat=4):
+        for orbs in product(self.all_orbs(), repeat=4):
+            indices, alphas, positions = zip(*orbs)
+            p, r, q, s = indices
+            self.Q[indices] = self.two_electron(alphas, positions, self.S[p, q], self.S[r, s])
+#            self.Q[p, r, q, s] = self.two_electron(alpha_p, alpha_q, alpha_r, alpha_s)
+
+    def kinetic(self, alpha, beta, fac, R_AB_sqrd):
+        term1 = fac * (3e0 - 2e0 * fac * R_AB_sqrd)
+        term2 = (pi / (alpha + beta)) ** (1.5e0)
+        term3 = exp(-fac * R_AB_sqrd)
+        return term1 * term2 * term3
+
+    def overlap(self, alpha, beta, fac, R_AB_sqrd):
+        return (pi / (alpha + beta)) ** (1.5e0) * exp(-fac * R_AB_sqrd)
+
+    def two_electron(self, alphas, positions, Spq, Srs):
+        alpha_p, alpha_r, alpha_q, alpha_s = alphas
+        A = alpha_p + alpha_q
+        B = alpha_r + alpha_s
+        R_p, R_r, R_q, R_s = positions
+        R_A = (alpha_p * R_p + alpha_q * R_q) / A
+        R_B = (alpha_r * R_r + alpha_s * R_s) / B
+        R_AB = R_A - R_B
+        R_AB_sqrd = dot(R_AB, R_AB)
+        t = A * B / (A + B) * R_AB_sqrd
+        Q = 2e0 * sqrt(A * B / (pi * (A + B))) * Spq * Srs * F0(t)
+        return Q
+
+    def variational(self, Cguess=False, tol=1e-8, max_iters=10):
+        if Cguess:
+            C = Cguess #  We should check that this vector is not zero
+        else:
+            C = ones(self.bsize)
+        #  Normalize the initial guess
+        C = 1e0 / sqrt(einsum('p,pq,q', C, self.S, C)) * C
+        #  Form the Fock matrix
+        def iteration(C):
+            F = self.H + einsum('ijkl,j,l', self.Q, C, C)
+            self.eigvals, self.eigvecs = eigh(F, self.S)
+            C = self.eigvecs[:, 0]
+            E0 = 2 * einsum('p,q,pq', C, C, self.H) + einsum('ijkl,i,k,j,l', self.Q, C, C, C, C)
+            print E0, einsum('p,pq,q', C, self.S, C), F.shape
+            return E0, C
+        E0_old, C_old = iteration(C)
+        diff = tol + 1e0
+        iters = 1
+        while (diff > tol) and (iters < max_iters):
+            E0_new, C_new = iteration(C_old)
+            iters += 1
+            diff = abs(E0_new - E0_old)
+            E0_old, C_old = E0_new, C_new
---
+++ thijssen/src/chap4/h2_plt.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+from numpy import array, linspace
+from chap4.h2 import H2, Atom
+
+
+#  Place the atoms one bohr radius apart
+R1 = array([-.5e0, 0e0, 0e0])
+R2 = -R1
+Z = 1e0
+exponents = array([13.00773, 1.962079, 0.444529, 0.1219492])
+left = Atom(Z, exponents, R1)
+right = Atom(Z, exponents, R2)
+h2 = H2((left, right))
+h2.variational()
+def print_energies():
+    for n in range(1, h2.bsize + 1):
+        approx = h2.energy_tot(n)
+        print('E{0} = {1:+.7e}'.format(n, approx))
+def plot_groundstate():
+    x = linspace(-3, 3, 100)
+    n = 1 #  Ground state
+    approx = h2.eigfunc(n, x)
+    plt.plot(x, approx, 'x-', label='STO-4G')
+    plt.xlabel(r'$x$ (where $y=z=0$)')
+    plt.ylabel(r'Ground state wave function')
+    plt.legend()
+    plt.title('Ground-state Electronic Eigenfunction of $H_{2}^{+}$ Molecule')
+    plt.show()
+#    plt.savefig('prob01Aplot.pdf')
+
+if __name__ == '__main__':
+    print_energies()
+#    plot_groundstate()
